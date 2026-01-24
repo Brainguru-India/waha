29fa181133d35920a879d7a46dece0c56a2b7c04
Brainguru Technologies Pvt. Ltd. <github@brainguru.in>
2026-01-24 15:19:03 +0530

    Create openwebui_with_nvidia_gpu.sh

---
+++ openwebui_with_nvidia_gpu.sh
@@ -0,0 +1,25 @@
+curl -fsSL https://raw.githubusercontent.com/Brainguru-India/waha/refs/heads/main/waha_basic_server_setup.sh | bash
+sudo apt install -y ubuntu-drivers-common
+sudo ubuntu-drivers autoinstall
+
+# -------------
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+  | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+  | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+  | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt update
+
+# -------------
+
+sudo apt install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+# -------------
+
+docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+ufw allow 3000
