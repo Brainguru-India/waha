56d84830dc63a4e8d4bb673824b729ef10f8d95f
Brainguru Technologies Pvt. Ltd. <github@brainguru.in>
2026-01-24 15:26:51 +0530

    Update openwebui_with_nvidia_gpu.sh

--- openwebui_with_nvidia_gpu.sh
+++ openwebui_with_nvidia_gpu.sh
@@ -21,4 +21,3 @@ sudo systemctl restart docker
 # -------------
 
 docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
-ufw allow 3000
